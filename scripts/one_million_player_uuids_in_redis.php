d8cea2848af820e68279a65accb40417230843d8
jim zike huang <iotpump.developer@gmail.com>
2017-04-24 13:48:48 -0700

    add key generation script

--- scripts/one_million_player_uuids_in_redis.php
+++ scripts/one_million_player_uuids_in_redis.php
@@ -1,10 +1,19 @@
 <?php
 $redis = new \Redis();
 $redis->connect('127.0.0.1',6379);
+$redis->flushAll();
 
 for ($i = 1; $i <= 1000000; $i++) {
     $uuid = uuid_create();
     $key = "player.$uuid.data";
     $value = "1";
     $redis->set($key, $value);
+}
+
+$keysPlayerData = [];
+
+$keysPlayerData = $redis->keys('player.*.data');
+
+foreach($keysPlayerData as $key => $val) {
+    echo (crc32($val) % 256)."\n";
 }
